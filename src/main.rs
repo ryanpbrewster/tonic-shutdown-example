72afb0325c7ea5637baee9a1c683f5e1149eaa0e
Ryan P. Brewster <RyanPBrewster@gmail.com>
2024-11-16 19:55:05 -0500

    Use a notify

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{sync::Arc, time::Duration};
 
 use clap::Parser;
 use tokio::signal::unix::{signal, SignalKind};
@@ -16,38 +16,45 @@ async fn main() -> anyhow::Result<()> {
         .register_encoded_file_descriptor_set(tonic_health::pb::FILE_DESCRIPTOR_SET)
         .build_v1()?;
 
-    let mut shutdown_signal = {
-        let (tx, rx) = tokio::sync::watch::channel(0);
+    // A signal that will be notified when the server should start shutting down
+    // For now, that can be triggered by sending a SIGINT.
+    let shutdown = {
+        let shutdown = Arc::new(tokio::sync::Notify::new());
+        let tx = shutdown.clone();
         let mut sig = signal(SignalKind::interrupt())?;
         tokio::spawn(async move {
             let _ = sig.recv().await;
-            tx.send_replace(1);
-            info!("waiting for grace period of 5s");
-            tokio::time::sleep(Duration::from_millis(5_000)).await;
-            info!("grace period complete, shutting down service");
-            tx.send_replace(2);
+            tx.notify_waiters();
         });
-        rx
+        shutdown
     };
 
     info!("server listening on {}", address);
-    let mut srv_sig = shutdown_signal.clone();
-    let graceful_exit = Server::builder()
+    let srv = Server::builder()
         .add_service(health_service)
-        .add_service(reflection_service)
-        .serve_with_shutdown(address.parse()?, async move {
-            let _ = srv_sig.wait_for(|&v| v == 1).await;
-            info!("shutdown requested, marking as unhealthy");
+        .add_service(reflection_service);
+
+    let graceful_exit = {
+        let shutdown = shutdown.clone();
+        srv.serve_with_shutdown(address.parse()?, async move {
+            shutdown.notified().await;
             health_reporter
                 .set_service_status("", tonic_health::ServingStatus::NotServing)
                 .await;
-        });
+        })
+    };
+
+    let ungraceful_exit = async move {
+        shutdown.notified().await;
+        info!("waiting up to 5s for clients to disconnect");
+        tokio::time::sleep(Duration::from_millis(5_000)).await;
+    };
 
     tokio::select! {
         _ = graceful_exit => {
             info!("gracefully exiting");
         }
-        _ = shutdown_signal.wait_for(|&v| v == 2) => {
+        _ = ungraceful_exit => {
             warn!("grace period exhausted, forcefully shutting down")
         }
     };
