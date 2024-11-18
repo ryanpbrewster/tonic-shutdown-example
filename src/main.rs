aa7919ee9061494e04df32248ddebb34f0c7b5e4
Ryan P. Brewster <RyanPBrewster@gmail.com>
2024-11-18 10:24:14 -0500

    fix error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use std::{sync::Arc, time::Duration};
+use std::{net::SocketAddr, time::Duration};
 
 use clap::Parser;
-use tokio::{
-    signal::unix::{signal, SignalKind},
-    sync::Semaphore,
-};
 use tonic::transport::Server;
 use tracing::{info, warn};
 
@@ -16,33 +12,42 @@ async fn main() -> anyhow::Result<()> {
         address,
         grace_period_ms,
     } = Args::parse();
+    let address: SocketAddr = address.parse()?;
 
     let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
     let reflection_service = tonic_reflection::server::Builder::configure()
         .register_encoded_file_descriptor_set(tonic_health::pb::FILE_DESCRIPTOR_SET)
         .build_v1()?;
 
-    let shutdown = shutdown_signal()?;
-
-    let graceful_exit = tokio::spawn({
-        let shutdown = shutdown.clone();
-        info!("server listening on {}", address);
-        let srv = Server::builder()
-            .add_service(health_service)
-            .add_service(reflection_service);
-        srv.serve_with_shutdown(address.parse()?, async move {
-            shutdown.wait().await;
-            info!("no longer accepting new connections");
-        })
+    let (tx, mut shutdown) = tokio::sync::watch::channel(false);
+    tokio::spawn(async move {
+        let _ = tokio::signal::ctrl_c().await;
+        info!("recv SIGINT, latching shutdown signal");
+        tx.send_replace(true);
     });
 
-    shutdown.wait().await;
-    info!("marking as unhealthy to discourage clients");
-    health_reporter
-        .set_service_status("", tonic_health::ServingStatus::NotServing)
-        .await;
+    // This future will resolve when the server shuts down organically (either via a graceful serve_with_shutdown
+    // or by encountering an error).
+    let organic = tokio::spawn({
+        let mut shutdown = shutdown.clone();
+        info!("server listening on {}", address);
+        Server::builder()
+            .add_service(health_service)
+            .add_service(reflection_service)
+            .serve_with_shutdown(address, async move {
+                let _ = shutdown.wait_for(|&is_shutdown| is_shutdown).await;
+                info!("marking as unhealthy to discourage clients");
+                health_reporter
+                    .set_service_status("", tonic_health::ServingStatus::NotServing)
+                    .await;
+                info!("no longer accepting new connections");
+            })
+    });
 
-    let ungraceful_exit = async move {
+    // This future will resolve after the process receives a SIGINT and the grace period has expired.
+    // When it resolves, we need to shut down ungracefully.
+    let ungraceful = async move {
+        let _ = shutdown.wait_for(|&is_shutdown| is_shutdown).await;
         if let Some(grace_period_ms) = grace_period_ms {
             info!("waiting up to {grace_period_ms}ms for clients to disconnect",);
             tokio::time::sleep(Duration::from_millis(grace_period_ms)).await;
@@ -53,49 +58,13 @@ async fn main() -> anyhow::Result<()> {
     };
 
     tokio::select! {
-        _ = graceful_exit => {
+        r = organic => {
+            r??; // if we hit any kind of organic error with the server, bubble that up
             info!("all clients gracefully disconnected, exiting");
-        }
-        _ = ungraceful_exit => {
-            warn!("grace period exhausted, forcefully shutting down connections")
-        }
+        },
+        _ = ungraceful => warn!("grace period exhausted, forcefully shutting down connections"),
     };
     Ok(())
-}
-
-// A signal that will be notified when the server should start shutting down
-// For now, that can be triggered by sending a SIGINT.
-fn shutdown_signal() -> anyhow::Result<Latch> {
-    let shutdown = Latch::new();
-    let tx = shutdown.clone();
-    let mut sig = signal(SignalKind::interrupt())?;
-    tokio::spawn(async move {
-        let _ = sig.recv().await;
-        info!("recv SIGINT, latching shutdown signal");
-        tx.latch();
-    });
-    Ok(shutdown)
-}
-
-// I'm sure there's some better async latch primitive around, but I haven't found one.
-// Things I have tried:
-//   - using tokio::sync::Notify: had to be super careful about missing notifications before calling `.notified()`
-//   - using tokio::sync::oneshot: can't clone the receiver, so it only works for a single waiter
-//   - using tokio::sync::watch: works fine, just awkward to have to store an initial value and use `wait_for`
-// The semaphore approach is a bit awkward as well. We initialize it with no permits, so `acquire()` will never
-// succeed. We close it when we want to wake waiters, and they'll wake by getting an error.
-#[derive(Clone)]
-struct Latch(Arc<Semaphore>);
-impl Latch {
-    fn new() -> Self {
-        Self(Arc::new(Semaphore::new(0)))
-    }
-    fn latch(&self) {
-        self.0.close();
-    }
-    async fn wait(&self) {
-        let _ = self.0.acquire().await;
-    }
 }
 
 #[derive(Parser)]
